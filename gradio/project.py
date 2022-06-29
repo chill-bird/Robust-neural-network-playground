c96ed2e908430921e8753da492fe05e1eee9bb59
KennyKuhn <97096399+KennyKuhn@users.noreply.github.com>
2022-06-29 12:26:56 +0200

    Add files via upload
    Connection to rendering-part added

--- gradio/project.py
+++ gradio/project.py
@@ -1,23 +1,22 @@
 import gradio as gr
 
-def show_picture():
-	return gr.Image(value="/home/kenny/Teamprojekt/flask_app/dummy.jpg", show_label=False)
+def picture():
+	return gr.Image("frame.png", show_label=False)
 
 def get_numbers(ratio, noise, size):
 	my_list = [ratio, noise, size]
-	return my_list[0]
+	return my_list
 
 slider_if = gr.Interface(get_numbers,
 	 	         inputs=[gr.Slider(10, 90, step="number"),
 			         gr.Slider(0, 50, step="number"),
 			         gr.Slider(1, 30, step="number", label="batch size")],
 		         outputs="text",
-		         live=True)
+                         live=True)
 
-dummy_image_if = gr.Interface(show_picture,
+dummy_image_if = gr.Interface(picture,
 	 		      inputs=[],
-			      outputs=show_picture(),
-			      live=True)
+			      outputs=picture())
 
 interface = gr.TabbedInterface([dummy_image_if, slider_if], ["Output", "Settings"])
 
