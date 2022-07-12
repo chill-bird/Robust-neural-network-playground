f4ebe140ba277d030f5c9ab54eb7d63983cfb3ab
KennyKuhn <97096399+KennyKuhn@users.noreply.github.com>
2022-07-12 17:48:15 +0200

    Add files via upload
    Cleaned code

--- interface.py
+++ interface.py
@@ -1,57 +1,35 @@
 import gradio as gr
 from datetime import datetime
+
 import neural_net
 
 def video():
 	return gr.Video("video.mp4")
 
-def picture():
-	return gr.Image("./frame.png", show_label=False)
-
-def loop(lr, bs):
+", layers, n1, n2, n3)"
+def loop(lr, bs, layers, n1, n2, n3):
 	lr = 1 / 10**lr
 	bs = 2**bs
-	lr = neural_net.set_learningRate(lr)
-	bs = neural_net.set_batch(bs)
+	neural_net.set_batch(bs)
+	neural_net.set_layers(layers)
+	neural_net.set_neurons1(n1)
+	neural_net.set_neurons2(n2)
+	neural_net.set_neurons3(n3)
+	neural_net.set_policy_net()
+	neural_net.set_learningRate(lr)
 	test_list = [lr, bs]
-	#return video()
-	return
+	neural_net.start_learning()
+	return test_list
 
-def get_numbers(ratio, noise, size):
-	my_list = [ratio, noise, size]
-	return my_list
-
-frame = render.render(0, 1, 10)
-frame.save("frame.png")
-#loop(0, 20)
-
-slider_if = gr.Interface(get_numbers,
-	 	         inputs=[gr.Slider(10, 90, step="number"),
-			         gr.Slider(0, 50, step="number"),
-			         gr.Slider(1, 30, step="number", label="batch size")],
-		         outputs="text",
-                         live=True)
-
-#loop_if = gr.Interface(loop,
-#		       inputs=[gr.Slider(-1, 1, step="number"),
-#			       gr.Slider(0, 360, step="number")],
-#		       outputs=["image"],
-#		       live=True)
-
-#dummy_image_if = gr.Interface(picture,
-#	 		      inputs=[gr.Slider(-1, 1, step="number"),
- #                             	      gr.Slider(0, 360, step="number")],
-#			      outputs=picture())
-			      #outputs=["image"],
-			      #live=True)
 
 video_if = gr.Interface(loop,
-			inputs=[gr.Slider(1, 6, step="number"), #label="learning rate (Wert = 1/(10^learning rate)"),
-                                gr.Slider(5, 10, step="number")], #label="batch size (Wert = 2^batch size)")],
+			inputs=[gr.Slider(1, 6, step="number", label="learning rate (Wert = 1/(10^learning rate)"),
+                                gr.Slider(5, 10, step="number", label="batch size (Wert = 2^batch size)"),
+								gr.Slider(1, 3, step="number", label="number of hidden layers in the network"),
+								gr.Slider(10, 128, step="number", label="number of nodes in first hidden layer"),
+								gr.Slider(10, 128, step="number", label="number of nodes in second hidden layer"),
+								gr.Slider(10, 128, step="number", label="number of nodes in third hidden layer")],
 			outputs="text")
 
-interface = gr.TabbedInterface([video_if, slider_if], ["Output", "Settings"])
+video_if.launch()
 
-#slider_if.launch()
-#dummy_image_if.launch()
-interface.launch()
